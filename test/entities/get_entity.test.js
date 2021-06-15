9205c2e4e8e3f29f7c8712a8ec46ad03db3148ba
Victor Bofill <victorm.bofill@gmail.com>
2021-06-15 10:14:16 -0700

    just showing britt some git shit

--- test/entities/get_entity.test.js
+++ test/entities/get_entity.test.js
@@ -1,30 +1,9 @@
 // third party packages
 const { expect } = require('chai');
 
-// local packages
-const { silaAPI } = require('../../index');
+describe.only('tests Sila API integration', () => {
+    it('shows britt how to use git!', async () => {
 
-// consts
-const { SILA_PATHS } = require('../../src/routes/index');
-const { USER_HANDLE_INDIVIDUAL_ONE } = require('../../.env');
-
-describe.skip('tests Sila API integration', () => {
-    it('/get_entity', async () => {
-        // prepare the request body
-        const body = {
-            apiPath: SILA_PATHS.GET_ENTITY,
-            data: {
-                userHandle: USER_HANDLE_INDIVIDUAL_ONE
-            }
-        }
-
-        const jsonBody = JSON.stringify(body);
-        const response = await silaAPI({ body: jsonBody });
-
-        const parsedResponse = JSON.parse(response.body);
-
-        console.log('parsedResponse: ', parsedResponse);
-
-        expect(parsedResponse.success).to.equal(true);
+        expect(1).to.equal(1);
     });
 });
